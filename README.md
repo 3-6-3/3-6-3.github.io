3b00064c69e47819582eca44eb1af674b7a21175
Zack <flufftuffrat@protonmail.com>
2026-08-24 20:48:19 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3-6-3.github.io
+# 3-6-3.github.io
---
+++ script.js
@@ -0,0 +1,23 @@
+function createCard(Image, Url) {
+    const newCardImage = document.createElement('img')
+    newCardImage.src = "Assets/" + Image
+    const Anchor = document.createElement('a')
+    Anchor.href = Url
+    Anchor.appendChild(newCardImage)
+    Anchor.className = "card"
+    document.getElementById("HubDiv").appendChild(Anchor)
+    const Boundaries = Anchor.getBoundingClientRect()
+    //Logic
+    Anchor.addEventListener('mousemove', (e) => {
+        
+        var relativeX = ((e.clientX - Boundaries.left) / Boundaries.width - 0.5) * -20
+        var relativeY = ((e.clientY - Boundaries.top) / Boundaries.height - 0.5) * 20
+        newCardImage.style.transform = `rotateX(${relativeY}deg) rotateY(${relativeX}deg)`
+    })
+
+    Anchor.addEventListener('mouseleave', (e) => {
+        newCardImage.style.removeProperty('transform')
+    })
+}
+
+createCard("Typescript-logo.png", "Typescript.html");
